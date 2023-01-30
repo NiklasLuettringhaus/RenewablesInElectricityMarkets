ef27d0fe5d1d52e47cc24a5f4837ed9f8a912766
NiklasLuettringhaus <niklas.luettringhaus@gmail.com>
2023-01-30 16:51:51 +0100

    setup

---
+++ env/Code/example.jl
@@ -0,0 +1,4 @@
+#using JuMP
+println("I am alive")
+using JuMP, Gurobi
+print("this is a path", pwd())
---
+++ env/test.jl
@@ -0,0 +1,6 @@
+println("this script is running")
+
+println("this is the path ", pwd())
+
+using JuMP
+
