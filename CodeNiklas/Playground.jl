5bf76204ca4fd0bb84d4931cbc40f89159f2b6f2
NiklasLuettringhaus <niklas.luettringhaus@gmail.com>
2023-01-31 21:41:33 +0100

    Update Playground.jl

--- CodeNiklas/Playground.jl
+++ CodeNiklas/Playground.jl
@@ -1,7 +1,18 @@
-
+#=
 for i in 1:10
-    println("iteation of $(i)")
+    display("iteation of $(i)")
 end
 
 
+["Iteration $(i)" for i in 1:10]
 
+=#
+#Functions
+function g(x::Int64)
+    x = x + 3
+    return x
+end
+
+f(x::Int64) = x + 3
+
+f(10)
