be5cfb32505f4a98fd818f01607ee5d1684dd067
Pauline <106803877+thuene163@users.noreply.github.com>
2023-02-19 17:23:51 +0100

    H2

--- Julia Exercices/A1_step_2.jl
+++ Julia Exercices/A1_step_2.jl
@@ -10,21 +10,28 @@ print(length(U_d))
 FN = Model(Gurobi.Optimizer)
 
 @variable(FN,p_d[t=1:T,d=1:D]>=0) #load of demand
-@variable(FN,p_w[t=1:T,w=1:W]>=0) #wind farm production
+@variable(FN,p_w_grid[t=1:T,w=1:W]>=0) #wind farm production for grid
+@variable(FN,p_w_H2[t=1:T,w=1:W]>=0) #wind farm production for H2
 @variable(FN,p_g[t=1:T,g=1:G]>=0) #power scheduled of generetor g
 @variable(FN,st[t=1:T,g=1:G]>=0, Bin) #if g starts in hour t
 @variable(FN,run[t=1:T,g=1:G]>=0, Bin) #if g is running in hour t
+@variable(FN,H2[t=1:T,h=1:2]>=0) #H2 prod
 
 @objective(FN, Max, sum(U_d[t,d]*p_d[t,d] for t=1:T,d=1:D)  #Revenue from demand
+            + H2_price*sum(p_w_H2[t,w]*H2_prod for t=1:T, w=1:2)
             - sum(run[t,g]*C_g[g]*p_g[t,g] for t=1:T,g=1:G) # Production cost conventional generator
-            - sum(0*p_w[t,w] for t=1:T,w=1:W)) #Maximize the social whalefare, /# Production cost Wind farm
-
+            - sum(0*(p_w_grid[t,w] + p_w_H2[t,w]) for t=1:T,w=1:W)) #Maximize the social whalefare, /# Production cost Wind farm
 
 @constraint(FN,[t=1:T,d=1:D], p_d[t,d] <= Cap_d[d]) #Demand limits constraint
 @constraint(FN,[t=1:T,g=1:G], p_g[t,g] <= Cap_g[g]) #Generation limits constraint
-@constraint(FN,[t=1:T,w=1:W], p_w[t,w] <= WF_prod[t,w]) #Weather-based limits constraint WF
-@constraint(FN, Balance[t=1:T], sum(p_d[t,d] for d=1:D) - sum(p_w[t,w] for w=1:W) - sum(run[t,g]*p_g[t,g] for g=1:G)==0) #Power balance constraint
+@constraint(FN,[t=1:T,w=1:W], (p_w_grid[t,w] + p_w_H2[t,w]) <= WF_prod[t,w]) #Weather-based limits constraint WF
+@constraint(FN,[t=1:T, w=1:2], p_w_H2[t,w] <= WF_cap[w]/2) #power from windfarm not above capacity of electrolyzer
+@constraint(FN,[t=1:T, w=3:W], p_w_H2[t,w] == 0) #set all other WF to 0 prod of H2
+@constraint(FN, Balance[t=1:T], sum(p_d[t,d] for d=1:D) - sum(p_w_grid[t,w] for w=1:W) - sum(run[t,g]*p_g[t,g] for g=1:G)==0) #Power balance constraint
 @constraint(FN, [t=1:T, g=1:G], st[t,g] >= run[t,g] - (t>1 ? run[t-1,g] : run[T,g]))
+
+@constraint(FN, [w=1:2], sum(p_w_H2[t,w]*H2_prod for t=1:T) >= H2_cap) # minimum of H2 prod WF 4
+
 
 #************************************************************************
 
@@ -43,7 +50,7 @@ if termination_status(FN) == MOI.OPTIMAL
 
     #Equilibrium price
     #This does not work because dual is not available for models with binaries
-    -dual.(Balance[:])
+    #-dual.(Balance[:])
     #Market clearing price
     println("Market clearing price:")
     #println(DA_price[t])  #Print equilibrium price
