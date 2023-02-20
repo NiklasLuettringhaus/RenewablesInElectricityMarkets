505e9686d8a2602e5a6162d5912da334446e67ab
DavidMench <124593762+DavidMench@users.noreply.github.com>
2023-02-20 16:11:17 +0100

    step 1 done

--- Julia Exercices/A1_step_1(DavidandJuan).jl
+++ Julia Exercices/A1_step_1(DavidandJuan).jl
@@ -28,7 +28,7 @@ FN = Model(Gurobi.Optimizer)
 @constraint(FN,[d=1:D], p_d[d] <= Cap_d[d]) #Demand limits constraint
 @constraint(FN,[g=1:G], p_g[g] <= Cap_g[g]) #Generation limits constraint
 @constraint(FN,[w=1:W], p_w[w] <= WF_prod[w]) #Weather-based limits constraint WF
-@constraint(FN, Balance, sum(p_d[d] fosr d=1:D) - sum(p_w[w] for w=1:W) - sum(p_g[g] for g=1:G)==0) #Power balance constraint
+@constraint(FN, Balance, sum(p_d[d] for d=1:D) - sum(p_w[w] for w=1:W) - sum(p_g[g] for g=1:G)==0) #Power balance constraint
 
 #print(FN) #print model to screen (only usable for small models)
 
