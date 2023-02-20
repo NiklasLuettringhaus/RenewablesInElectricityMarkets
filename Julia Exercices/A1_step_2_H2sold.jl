a989d36b919e4c57ab20fe6114abdea2bf3d0138
Pauline <106803877+thuene163@users.noreply.github.com>
2023-02-20 21:15:01 +0100

    H2

--- Julia Exercices/A1_step_2_H2sold.jl
+++ Julia Exercices/A1_step_2_H2sold.jl
@@ -18,10 +18,12 @@ FN = Model(Gurobi.Optimizer)
 
 @objective(FN, Max, sum(U_d[t,d]*p_d[t,d] for t=1:T,d=1:D)  #Revenue from demand
             - sum(C_g[g]*p_g[t,g] for t=1:T,g=1:G) # Production cost conventional generator
-            - sum(DA_price[t]*p_w_H2[t,w] for t=1:T,w=1:2)) #Maximize the social whalefare, /# Production cost Wind farm
+            - sum(DA_price[t]*p_w_H2[t,w] for t=1:T, w=1:2) # production cost of H2 > doesn't work
+            + sum(H2_price*p_w_H2[t,w] for t=1:T, w=1:2)) #Maximize the social whalefare, /# Production cost Wind farm
+
 
 #Capacity limits
-@constraint(FN,[t=1:T,d=1:D], p_d[t,d] <= Cap_d[d]) #Demand limits constraint
+@constraint(FN,[t=1:T,d=1:D], p_d[t,d] <= Cap_d[t,d]) #Demand limits constraint
 @constraint(FN,[t=1:T,g=1:G], p_g[t,g] <= Cap_g[g]) #Generation limits constraint
 @constraint(FN,[t=1:T,w=1:W], p_w_grid[t,w] + p_w_H2[t,w] <= WF_prod[t,w]) #Weather-based limits constraint WF
 
@@ -29,14 +31,12 @@ FN = Model(Gurobi.Optimizer)
 @constraint(FN, Balance[t=1:T], sum(p_d[t,d] for d=1:D) - sum(p_w_grid[t,w] for w=1:W) - sum(p_g[t,g] for g=1:G)==0) #Power balance constraint
 
 #Ramping up and down constraints
-@constraint(FN,[t=1:T,g=1:G], p_g[t,g] <= (t=1 ? Cap_g_init[g] : p_g[t-1,g]) + Ramp_g_u[g]) #ramp up constraint
-@constraint(FN,[t=1:T,g=1:G], p_g[t,g] >= (t=1 ? Cap_g_init[g] : p_g[t-1,g]) + Ramp_g_d[g]) #ramp down constraint
+@constraint(FN,[t=1:T,g=1:G], p_g[t,g] <= (t-1<1 ? Cap_g_init[g] : p_g[t-1,g]) + Ramp_g_u[g]) #ramp up constraint
+@constraint(FN,[t=1:T,g=1:G], p_g[t,g] >= (t-1<1 ? Cap_g_init[g] : p_g[t-1,g]) - Ramp_g_d[g]) #ramp down constraint
 
 #Electrolyzer constraints
-@constraint(FN,[t=1:T,w=1:2], p_w_H2[t,w] <= WF_cap[w]/2)
-@constraint(FN,[w=1:2], sum(p_w_H2[t,w]*H2_prod for t=1:T) >= H2_cap)
-
-
+@constraint(FN,[t=1:T, w=1:2], p_w_H2[t,w] <= WF_cap[w]/2)
+@constraint(FN,[t=1:T, w=1:2], sum(p_w_H2[t,w]*H2_prod for t=1:T) >= H2_cap)
 
 #************************************************************************
 
@@ -52,8 +52,12 @@ if termination_status(FN) == MOI.OPTIMAL
     println("Optimal objective value: $(objective_value(FN))")
     println("Solution: ")
 
+
+
     #Equilibrium price
     DA_price = -dual.(Balance[:])
+
+    println(value(sum(DA_price[t]*p_w_H2[t,w] for t=1:T, w=1:2)))
     #Market clearing price
     println("Market clearing price:")
     for t=1:T
@@ -67,16 +71,28 @@ if termination_status(FN) == MOI.OPTIMAL
     end
     println("\n")
 
-    println("Daily profit of windfarms:")
-    for w=1:W
-        println("WF $w: ", round(Int,value(sum(p_w[t,w]*DA_price[t] for t=1:T))))
+    println("Daily production of each generator:")
+    for g=1:G
+        println("G$g: ", round(Int,value(sum(p_g[t,g] for t=1:T))))
     end
     println("\n")
 
-    println("Utility of each demand:")
-    for d=1:D
-        println("D$d: ", round(Int, value(sum(p_d[t,d]*(U_d[d] - DA_price[t]) for t=1:T))))
+    println("Daily profit of windfarms:")
+    for w=1:W
+        println("WF $w: ", round(Int,value(sum(p_w_grid[t,w]*DA_price[t] for t=1:T))))
     end
+    println("\n")
+
+    println("Daily production of hydrogen:")
+    for w=1:2
+        println("WF $w: ", round(Int,value(sum(p_w_H2[t,w]*H2_prod for t=1:T))))
+    end
+    println("\n")
+
+    #println("Utility of each demand:")
+    #for d=1:D
+    #    println("D$d: ", round(Int, value(sum(p_d[t,d]*(U_d[d] - DA_price[t]) for t=1:T))))
+    #end
 
 else
     println("No optimal solution available")
---
+++ Julia Exercices/A1_step_2_noH2sold.jl
@@ -0,0 +1,96 @@
+#************************************************************************
+# Incredible Chairs, Simple LP
+using JuMP
+using Gurobi
+
+
+include("data_Step_2.jl")
+
+#************************************************************************
+# Model
+FN = Model(Gurobi.Optimizer)
+
+@variable(FN,p_d[t=1:T,d=1:D]>=0) #load of demand
+@variable(FN,p_w_grid[t=1:T,w=1:W]>=0) #wind farm production to grid
+@variable(FN,p_w_H2[t=1:T,w=1:W]>=0) #wind farm production to electrolyzer
+@variable(FN,p_g[t=1:T,g=1:G]>=0) #power scheduled of generetor g
+
+
+@objective(FN, Max, sum(U_d[t,d]*p_d[t,d] for t=1:T,d=1:D)  #Revenue from demand
+            - sum(C_g[g]*p_g[t,g] for t=1:T,g=1:G) # Production cost conventional generator
+            - sum(DA_price[t]*p_w_H2[t,w] for t=1:T,w=1:2)) # Production cost H2 > doesn't work
+
+#Capacity limits
+@constraint(FN,[t=1:T,d=1:D], p_d[t,d] <= Cap_d[t,d]) #Demand limits constraint
+@constraint(FN,[t=1:T,g=1:G], p_g[t,g] <= Cap_g[g]) #Generation limits constraint
+@constraint(FN,[t=1:T,w=1:W], p_w_grid[t,w] + p_w_H2[t,w] <= WF_prod[t,w]) #Weather-based limits constraint WF
+
+#Power Balance
+@constraint(FN, Balance[t=1:T], sum(p_d[t,d] for d=1:D) - sum(p_w_grid[t,w] for w=1:W) - sum(p_g[t,g] for g=1:G)==0) #Power balance constraint
+
+#Ramping up and down constraints
+@constraint(FN,[t=1:T,g=1:G], p_g[t,g] <= (t-1<1 ? Cap_g_init[g] : p_g[t-1,g]) + Ramp_g_u[g]) #ramp up constraint
+@constraint(FN,[t=1:T,g=1:G], p_g[t,g] >= (t-1<1 ? Cap_g_init[g] : p_g[t-1,g]) - Ramp_g_d[g]) #ramp down constraint
+
+#Electrolyzer constraints
+@constraint(FN,[t=1:T, w=1:2], p_w_H2[t,w] <= WF_cap[w]/2)
+@constraint(FN,[t=1:T, w=1:2], sum(p_w_H2[t,w]*H2_prod for t=1:T) >= H2_cap)
+
+
+
+#************************************************************************
+
+#************************************************************************
+# Solve
+solution = optimize!(FN)
+println("Termination status: $(termination_status(FN))")
+#************************************************************************
+
+#************************************************************************
+# Solution
+if termination_status(FN) == MOI.OPTIMAL
+    println("Optimal objective value: $(objective_value(FN))")
+    println("Solution: ")
+
+    #Equilibrium price
+    DA_price = -dual.(Balance[:])
+    #Market clearing price
+    println("Market clearing price:")
+    for t=1:T
+        println("Hour $t: ", value(DA_price[t])) #Print equilibrium price
+    end   
+
+    println("\n")
+    println("Daily profit of each generator:")
+    for g=1:G
+        println("G$g: ", round(Int,value(sum(p_g[t,g]*(DA_price[t] - C_g[g]) for t=1:T))))
+    end
+    println("\n")
+
+    println("Daily production of each generator:")
+    for g=1:G
+        println("G$g: ", round(Int,value(sum(p_g[t,g] for t=1:T))))
+    end
+    println("\n")
+
+    println("Daily profit of windfarms:")
+    for w=1:W
+        println("WF $w: ", round(Int,value(sum(p_w_grid[t,w]*DA_price[t] for t=1:T))))
+    end
+    println("\n")
+
+    println("Daily production of hydrogen:")
+    for w=1:2
+        println("WF $w: ", round(Int,value(sum(p_w_H2[t,w]*H2_prod for t=1:T))))
+    end
+    println("\n")
+
+    #println("Utility of each demand:")
+    #for d=1:D
+    #    println("D$d: ", round(Int, value(sum(p_d[t,d]*(U_d[d] - DA_price[t]) for t=1:T))))
+    #end
+
+else
+    println("No optimal solution available")
+end
+#************************************************************************
