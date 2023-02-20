6ac772c404886eb4fd739c8b0a4725c08954f975
Pauline <106803877+thuene163@users.noreply.github.com>
2023-02-20 16:28:43 +0100

    H2 + ramp constraint

--- Julia Exercices/A1_step_2_noH2.jl
+++ Julia Exercices/A1_step_2_noH2.jl
@@ -11,20 +11,32 @@ include("data_Step_2.jl")
 FN = Model(Gurobi.Optimizer)
 
 @variable(FN,p_d[t=1:T,d=1:D]>=0) #load of demand
-@variable(FN,p_w[t=1:T,w=1:W]>=0) #wind farm production
+@variable(FN,p_w_grid[t=1:T,w=1:W]>=0) #wind farm production to grid
+@variable(FN,p_w_H2[t=1:T,w=1:W]>=0) #wind farm production to electrolyzer
 @variable(FN,p_g[t=1:T,g=1:G]>=0) #power scheduled of generetor g
+
 
 @objective(FN, Max, sum(U_d[t,d]*p_d[t,d] for t=1:T,d=1:D)  #Revenue from demand
             - sum(C_g[g]*p_g[t,g] for t=1:T,g=1:G) # Production cost conventional generator
-            - sum(0*p_w[t,w] for t=1:T,w=1:W)) #Maximize the social whalefare, /# Production cost Wind farm
+            - sum(DA_price[t]*p_w_H2[t,w] for t=1:T,w=1:2)) #Maximize the social whalefare, /# Production cost Wind farm
 
-
+#Capacity limits
 @constraint(FN,[t=1:T,d=1:D], p_d[t,d] <= Cap_d[d]) #Demand limits constraint
 @constraint(FN,[t=1:T,g=1:G], p_g[t,g] <= Cap_g[g]) #Generation limits constraint
-@constraint(FN,[t=1:T,w=1:W], p_w[t,w] <= WF_prod[t,w]) #Weather-based limits constraint WF
-@constraint(FN, Balance[t=1:T], sum(p_d[t,d] for d=1:D) - sum(p_w[t,w] for w=1:W) - sum(p_g[t,g] for g=1:G)==0) #Power balance constraint
+@constraint(FN,[t=1:T,w=1:W], p_w_grid[t,w] + p_w_H2[t,w] <= WF_prod[t,w]) #Weather-based limits constraint WF
 
-#print(FN) #print model to screen (only usable for small models)
+#Power Balance
+@constraint(FN, Balance[t=1:T], sum(p_d[t,d] for d=1:D) - sum(p_w_grid[t,w] for w=1:W) - sum(p_g[t,g] for g=1:G)==0) #Power balance constraint
+
+#Ramping up and down constraints
+@constraint(FN,[t=1:T,g=1:G], p_g[t,g] <= (t=1 ? Cap_g_init[g] : p_g[t-1,g]) + Ramp_g_u[g]) #ramp up constraint
+@constraint(FN,[t=1:T,g=1:G], p_g[t,g] >= (t=1 ? Cap_g_init[g] : p_g[t-1,g]) + Ramp_g_d[g]) #ramp down constraint
+
+#Electrolyzer constraints
+@constraint(FN,[t=1:T,w=1:2], p_w_H2[t,w] <= WF_cap[w]/2)
+@constraint(FN,[w=1:2], sum(p_w_H2[t,w]*H2_prod for t=1:T) >= H2_cap)
+
+
 
 #************************************************************************
 
