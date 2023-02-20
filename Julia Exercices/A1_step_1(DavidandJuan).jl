c97abdf5acc8381ee7f05bdd94d2153f36ed95ee
juanfebbk <s184356@student.dtu.dk>
2023-02-20 15:59:07 +0100

    Update A1_step_1(DavidandJuan).jl
    NO start up cost

--- Julia Exercices/A1_step_1(DavidandJuan).jl
+++ Julia Exercices/A1_step_1(DavidandJuan).jl
@@ -39,10 +39,9 @@ FN = Model(Gurobi.Optimizer)
 @variable(FN,p_d[d=1:D]>=0) #load of demand
 @variable(FN,p_w[w=1:W]>=0) #wind farm production
 @variable(FN,p_g[g=1:G]>=0) #power scheduled of generetor g
-@variable(FN,act_g[g=1:G],Bin) #Binary variable: 1 if generator is active, 0 otherwise
 
 @objective(FN, Max, sum(U_d[d]*p_d[d] for d=1:D)  #Revenue from demand
-            - sum(C_g[g]*p_g[g] + C_st[g] for g=1:G) # Production cost + start-up cost conventional generator
+            - sum(C_g[g]*p_g[g] for g=1:G) # Production cost + start-up cost conventional generator
             - sum(0*p_w[w] for w=1:W)) #Maximize the social whalefare, /# Production cost Wind farm
 
 
@@ -51,7 +50,7 @@ FN = Model(Gurobi.Optimizer)
 @constraint(FN,[w=1:W], p_w[w] <= WF_prod[w]) #Weather-based limits constraint WF
 @constraint(FN, Balance, sum(p_d[d] for d=1:D) - sum(p_w[w] for w=1:W) - sum(p_g[g] for g=1:G)==0) #Power balance constraint
 
-print(FN) #print model to screen (only usable for small models)
+#print(FN) #print model to screen (only usable for small models)
 
 #************************************************************************
 
@@ -73,6 +72,11 @@ if termination_status(FN) == MOI.OPTIMAL
     println("Profit of each generator:")
     for g=1:G
         println("G$g:", round(Int,value(p_g[g])*(DA_price - C_g[g])))
+    end
+    println("\n")
+    println("Profit of each wind farm:")
+    for w=1:W
+        println("G$w:", round(Int,value(p_w[w])*(DA_price - 0)))
     end
     println("\n")
     println("Utility of each demand:")
