302ce1d8798bf00c79731ea629f0e1603c6183b0
Pauline <106803877+thuene163@users.noreply.github.com>
2023-03-14 10:08:42 +0100

    Electrolyser corected

--- Julia Exercices/A1_step_4_2_H2_zonal.jl
+++ Julia Exercices/A1_step_4_2_H2_zonal.jl
@@ -13,7 +13,7 @@ import XLSX
 
 #************************************************************************
 #PARAMETERS
-include("data_Step_4.jl")
+include("data_Step_4_2.jl")
 
 #************************************************************************
 
@@ -21,59 +21,43 @@ include("data_Step_4.jl")
 # Model
 FN = Model(Gurobi.Optimizer)
 
-@variable(FN, p_d[d=1:D]>=0) #load of demand
+@variable(FN, p_d[d=1:D]>=0) #covered demand DA
 
 @variable(FN, up_bal[g=1:G]>=0) # up balancing action of gens
 @variable(FN, down_bal[g=1:G]>=0) # down balancing action of gens
-@variable(FN, wind_cur[w=1:W]>=0) #wind farm curtailment if electrolyzer
-@variable(FN, outage[g=1:G]>=0)    #
 
 @variable(FN,p_w_grid_DA[w=1:W]>=0) #wind farm production to grid from DA
 @variable(FN,p_w_H2_DA[w=1:W]>=0) #wind farm production to electrolyzer from DA
 
-@variable(FN,p_g[g=1:G]>=0) #power scheduled of generetor g
+@variable(FN,p_g[g=1:G]>=0) #power scheduled DA of generetor g
 
 
 
-@objective(FN, Max, sum(U_d[d]*p_d[d] for d=1:D)              #Revenue from demand
-            #- sum(cost_load_cur*load_cur[d] for d=1:D)         #curtailment cost load
-            - sum(C_g[g]*p_g[g] for g=1:G)                      #Production cost + start-up cost conventional generator
-            - sum(up_bal[g]*(DA_price+0.12*C_g[g]) for g=1:G)         #cost for upbalancing > lowering consumption
+@objective(FN, Max, sum(U_d[d]*p_d[d] for d=1:D)                    #Revenue from demand
+            - sum(C_g[g]*p_g[g] for g=1:G)                          #Production cost + start-up cost conventional generator
+            - sum(up_bal[g]*(DA_price+0.12*C_g[g]) for g=1:G)       #cost for upbalancing > lowering consumption
             - sum(down_bal[g]*(DA_price-0.15*C_g[g]) for g=1:G))    #cost for downbalancing > increasing consumption
 
 #Capacity Limits
 @constraint(FN,[d=1:D], p_d[d] <= Cap_d[d]) #Demand limits constraint
+#@constraint(FN,[g=1:G], p_g[g] <= Cap_g[g]) #Generation limits constraint
 @constraint(FN,[g=1:G], p_g[g] + up_bal[g] <= Cap_g[g]) #Generation limits constraint
 @constraint(FN,[g=1:G], p_g[g] - down_bal[g] >= 0) #Generation limits constraint
 @constraint(FN,[w=1:W], p_w_grid_DA[w] + p_w_H2_DA[w] <= transpose(WF_prod[w])) #Weather-based limits constraint WF from DA market
 
 # DA Power Balance
-@constraint(FN, Balance, sum(p_d[d] for d=1:D) 
-                                #+ sum(f[a,b] for b=1:A if ATC[a,b]>0) 
-                                - sum(p_w_grid_DA[w] for w=1:W) 
-                                - sum(p_g[g] for g=1:G)
-                                ==0)
-
-#Ramping up and down constraints
-#@constraint(FN,[g=1:G], p_g[g] <= (t-1<1 ? Cap_g_init[g] : p_g[t-1,g]) + Ramp_g_u[g]) #ramp up constraint
-#@constraint(FN,[g=1:G], p_g[g] >= (t-1<1 ? Cap_g_init[g] : p_g[t-1,g]) - Ramp_g_d[g]) #ramp down constraint
-
-#=
-#Power Flow constraints
-@constraint(FN,[a=1:A,b=1:A],f[a,b]<=ATC[a,b]) # Max ATC connecting zones a to b
-@constraint(FN,[a=1:A,b=1:A],f[a,b]>=-ATC[b,a]) # Min ATC connecting zones a to b
-@constraint(FN,[a=1:A,b=1:A],f[a,b]==-f[b,a]) #Symmetrical flow
-=#
+@constraint(FN, Balance, sum(p_d[d] for d=1:D) - sum(p_w_grid_DA[w] for w=1:W) - sum(p_g[g] for g=1:G) ==0)
 
 #Electrolyzer constraints
-@constraint(FN,[ w=1:2], 0.01*(WF_cap[w]/2) <= p_w_H2_DA[w] <= WF_cap[w]/2)
-@constraint(FN,[ w=1:2], sum(p_w_H2_DA[w]*H2_prod for t=1:T) >= H2_cap)
+@constraint(FN,[w=1:2], 0.01*(WF_cap[w]/2) <= p_w_H2_DA[w] <= WF_cap[w]/2)
+@constraint(FN,[w=1:2], sum(p_w_H2_DA[w]*H2_prod for t=1:T) >= H2_cap)
 
 #Balancing action
-#@constraint(FN, [t=1:T], outage[7] == p_g[7])                   #outage of generator 7
-#@constraint(FN, [t=1: g=1,2,3,4,5,6,8,9,10,11,12], outage[g] == 0)     #
-@constraint(FN, [w=1:W; outage_sum >= 0], outage_sum <= sum(wind_cur[w] for w=1:W) + sum(down_bal[g] for g=1:G))    #down balancing action of electrolyzer needs to match WF error
-@constraint(FN, [w=1:W; outage_sum < 0], abs(outage_sum) <= sum(up_bal[g] for g=1:G))  #up balancing action of electrolyzer needs to match WF error
+@constraint(FN, [w=1:W; outage_sum >= 0], outage_sum == sum(down_bal[g] for g=1:G))    #down balancing action of electrolyzer needs to match WF error
+@constraint(FN, [w=1:W; outage_sum < 0], abs(outage_sum) == sum(up_bal[g] for g=1:G))  #up balancing action of electrolyzer needs to match WF error
+@constraint(FN, p_g[9] == Cap_g[9])
+@constraint(FN, down_bal[9] == 0)
+@constraint(FN, up_bal[9] == 0)
 
 #print(FN) #print model to screen (only usable for small models)
 
@@ -91,108 +75,116 @@ if termination_status(FN) == MOI.OPTIMAL
     println("Optimal objective value: $(objective_value(FN))")
     println("Solution: ")
     DA_price = -dual.(Balance) #Equilibrium price
+    #Up_price = -dual.(Balance_up) #Equilibrium price
+    #Down_price = -dual.(Balance_down) #Equilibrium price
 
-    println("Cost of hydrogen production: ", round(value(sum(DA_price[t]*p_w_H2_DA[w] for w=1:2)), digits = 2))
-    println("Cost of up balancing: ", round(value(sum(up_bal_w_H2[w]*0.85*DA_price[t] for w=1:2)), digits = 2))
-    println("Cost of down balancing: ", round(value(sum(down_bal_w_H2[w]*1.1*DA_price[t] for w=1:2)), digits = 2))
+    println("Cost of hydrogen production: ", round(value(sum(DA_price*p_w_H2_DA[w] for w=1:2)), digits = 2))
+    #println("Cost of up balancing: ", round(value(sum(up_bal[g]*(DA_price+0.12*C_g[g]) for g=1:G)), digits = 2))
+    #println("Cost of down balancing: ", round(value(sum(down_bal[g]*(DA_price-0.15*C_g[g]) for g=1:G)), digits = 2))
+
     #Market clearing price
-    #=println("Market clearing price:")
-    for t=1:T
-        println("Hour $t: ", value(DA_price[t])) #Print equilibrium price
-    end   
+    println("Market clearing price:", value(DA_price))#Print equilibrium price
+    #println("Up balancing clearing price:", value(Up_price))#Print equilibrium price
+    #println("Down balancing clearing price:", value(Down_price))#Print equilibrium price
+
 
     println("\n")
-    println("Daily profit of each generator:")
+    println("Profit of each generator:")
     for g=1:G
-        println("G$g: ", round(Int,value(sum(p_g[t,g]*(DA_price[t] - C_g[g]) for t=1:T))))
+        println("G$g: ", round(Int,value(p_g[g]*(DA_price - C_g[g]) + up_bal[g]*(DA_price+0.12*C_g[g]) + down_bal[g]*(DA_price-0.15*C_g[g]))))
+        if g==9
+        println("G$g: ", round(Int,value(p_g[g]*(DA_price - C_g[g])
+                                        -sum(up_bal[g]*(DA_price+0.12*C_g[g]) for g=1:G)
+                                        -sum(down_bal[g]*(DA_price-0.15*C_g[g]) for g=1:G))))
+        end
     end
     println("\n")
 
-    println("Daily production of each generator:")
+    println("Scheduled DA production of each generator:")
     for g=1:G
-        println("G$g: ", round(Int,value(sum(p_g[t,g] for t=1:T))))
+        println("G$g: ", round(Int,value(p_g[g])))
     end
     println("\n")
 
-    println("Daily demand:")
+    println("Up Balancing of each generator:")
+    for g=1:G
+        println("G$g: ", round(Int,value(up_bal[g])))
+    end
+    println("\n")
+
+    println("Down Balancing of each generator:")
+    for g=1:G
+        println("G$g: ", round(Int,value(down_bal[g])))
+    end
+    println("\n")
+
+    println("Demand:")
     for d=1:D
-        println("D$d: ", round(Int,value(sum(p_d[t,d] for t=1:T))))
+        println("D$d: ", round(Int,value(p_d[d])))
     end
     println("\n")
 
-    println("Daily profit of windfarms:")
+    println("Profit of windfarms:")
     for w=1:W
-        println("WF $w: ", round(Int,value(sum(p_w_grid_DA[t,w]*DA_price[t] for t=1:T))))
+        println("WF $w: ", round(Int,value(p_w_grid_DA[w]*DA_price)))
     end
     println("\n")
 
-    println("Daily production of windfarms:")
+    println("Production of windfarms:")
     for w=1:W
-        println("Grid WF $w: ", round(Int,value(sum(p_w_grid_DA[t,w] for t=1:T))))
-        println("H2 WF $w: ", round(Int,value(sum(p_w_H2_DA[t,w] for t=1:T))))
+        println("Grid WF $w: ", round(Int,value(p_w_grid_DA[w])))
+        for w=1:2
+        println("H2 WF $w: ", round(Int,value(p_w_H2_DA[w])))
+        end
     end
     println("\n")
 
-    println("Daily production of hydrogen:")
+    println("Production of hydrogen:")
     for w=1:2
-        println("WF $w: ", round(Int,value(sum(p_w_H2_DA[t,w]*H2_prod for t=1:T))))
+        println("WF $w: ", round(Int,value(p_w_H2_DA[w]*H2_prod)))
     end
     println("\n")
 
     println("Fulfilled demand:")
-    for t=1:T
-            println("Hour $t: ", round(value((sum(p_d[t,d] for d=1:D)/sum(Cap_d[d] for d=1:D))*100),digits=2), "%")
-    end
-
+    println(round(value((sum(p_d[d] for d=1:D)/sum(Cap_d[d] for d=1:D))*100),digits=2), "%")
 
     println("\n")
-    DA_price_df=DataFrame(DA_price,areas)
-    Flows_df=DataFrame(value.(f[1, :, :]),areas)
-    PG_df=DataFrame(value.(p_g[:, :]),:auto)
-    PD_df=DataFrame(value.(p_d[:, :]),:auto)
-    PW_Grid_df=DataFrame(value.(p_w_grid_DA[:, :]),:auto)
-    PG_zonal_df=DataFrame(value.(p_g[:, :])*psi_g*psi_n,areas)
-    PD_zonal_df=DataFrame(value.(p_d[:, :])*psi_d*psi_n,areas)
-    PW_Grid_zonal_df=DataFrame(value.(p_w_grid_DA[:, :])*psi_w*psi_n,areas)
+    
+    #=
+    DA_price_df=DataFrame(DA_price)
+    PG_df=DataFrame(value.(p_g[:]),:auto)
+    PD_df=DataFrame(value.(p_d[:]),:auto)
+    PW_Grid_df=DataFrame(value.(p_w_grid_DA[:]),:auto)
 
-    Hydrogen_Prodcution_Day_ahead_df=DataFrame(value.(p_w_H2_DA[: , :]), Wind_turbines)
-    Down_Blancing_H2_df=DataFrame(value.(down_bal_w_H2[:, :]),Wind_turbines )
-    Up_Blancing_H2_df=DataFrame(value.(up_bal_w_H2[:, :]), Wind_turbines)
-    Load_Curtailment_df=DataFrame(value.(load_cur[:, :]), vec(Loads))
-    Lindt_Curtailment_df=DataFrame(value.(wind_cur[:, :]), Wind_turbines)                   #Rittersport is better anyway
-
-
+    Hydrogen_Prodcution_Day_ahead_df=DataFrame(value.(p_w_H2_DA[:]), Wind_turbines)
+    Down_Blancing_df=DataFrame(value.(down_bal[:]), Generators)
+    Up_Blancing_df=DataFrame(value.(up_bal[:, :]), Generators)             
+=#
 else
     println("No optimal solution available")
-=#
+
 end
 
 #=
-println(Load_Curtailment_df)
+println(Down_Blancing_df)
 #************************************************************************
 
 #**************************
-if(isfile("results_step4_zonal.xlsx"))
-    rm("results_step4_zonal.xlsx")
+
+if(isfile("results_step4_2.xlsx"))
+    rm("results_step4_2.xlsx")
 end
 
-XLSX.writetable("results_step4_H2_zonal.xlsx",
+XLSX.writetable("results_step4_2.xlsx",
     DA_Prices = (collect(eachcol(DA_price_df)), names(DA_price_df)),
-    Flows = (collect(eachcol(Flows_df)), names(Flows_df)),
     Generation = (collect(eachcol(PG_df)), names(PG_df)),
     Demand=(collect(eachcol(PD_df)), names(PD_df)),
     Wind_to_Grid=(collect(eachcol(PW_Grid_df)), names(PW_Grid_df)),
-    Zonal_Generation=(collect(eachcol(PG_zonal_df)), names(PG_zonal_df)),
-    Zonal_Demand=(collect(eachcol(PD_zonal_df)), names(PD_zonal_df)),
-    Zonal_Wind=(collect(eachcol(PW_Grid_zonal_df)), names(PW_Grid_zonal_df)),
 
     Hydrogen_Prodcution_Day_ahead=(collect(eachcol(Hydrogen_Prodcution_Day_ahead_df)), names(Hydrogen_Prodcution_Day_ahead_df)),
-    Down_Blancing_H2=(collect(eachcol(Down_Blancing_H2_df)), names(Down_Blancing_H2_df)),
-    Up_Blancing_H2=(collect(eachcol(Up_Blancing_H2_df)), names(Up_Blancing_H2_df)),
-    Lindt_Curtailment=(collect(eachcol(Lindt_Curtailment_df)), names(Lindt_Curtailment_df)), #no chocolate for you
-    Load_Curtailment=(collect(eachcol(Load_Curtailment_df)), names(Load_Curtailment_df)),
+    Down_Blancing=(collect(eachcol(Down_Blancing_df)), names(Down_Blancing_df)),
+    Up_Blancing=(collect(eachcol(Up_Blancing_df)), names(Up_Blancing_df)),
 
     )
-        =#
-
+=#
 #*****************************************************
