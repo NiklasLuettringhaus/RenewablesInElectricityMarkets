10bd22766a8a3cd9e229d4f2228b107555deccd9
NiklasLuettringhaus <niklas.luettringhaus@gmail.com>
2023-03-17 15:06:19 +0100

    Step 5.2 base

--- Julia Exercices/A1_step_5_Day_Ahead_Market.jl
+++ Julia Exercices/A1_step_5_Day_Ahead_Market.jl
@@ -23,45 +23,32 @@ FN = Model(Gurobi.Optimizer)
 
 @variable(FN,p_d[t=1:T,d=1:D]>=0) #load of demand
 
-@variable(FN,up_bal_w_H2[t=1:T,w=1:W]>=0) # up balancing action of electrolyzers
-@variable(FN,down_bal_w_H2[t=1:T,w=1:W]>=0) # down balancing action of electrolyzers
-@variable(FN, wind_cur[t=1:T, w=1:W]>=0) #wind farm curtailment if electrolyzer
-@variable(FN, load_cur[t=1:T, d=1:D]>=0) #load curtailment
-
 @variable(FN,p_w_grid_DA[t=1:T,w=1:W]>=0) #wind farm production to grid from DA
 @variable(FN,p_w_H2_DA[t=1:T,w=1:W]>=0) #wind farm production to electrolyzer from DA
 
 @variable(FN,p_g[t=1:T,g=1:G]>=0) #power scheduled of generetor g
-@variable(FN,f[t=1:T,a=1:A,b=1:A]) #DC flows between nodes n to m
-
-
 
 @objective(FN, Max, sum(U_d[t,d]*p_d[t,d] for t=1:T,d=1:D)              #Revenue from demand
             - sum(cost_load_cur*load_cur[t,d] for t=1:T, d=1:D)         #curtailment cost load
             - sum(C_g[g]*p_g[t,g] for t=1:T,g=1:G)                      #Production cost + start-up cost conventional generator
-            - sum(up_bal_w_H2[t,w]*0.85*DA_price[t] for t=1:T, w=1:2)   #cost for upbalancing > lowering consumption
-            - sum(down_bal_w_H2[t,w]*1.1*DA_price[t] for t=1:T, w=1:2)) #cost for downbalancing > increasing consumption
+
+            - sum(up_g[t,g] * c_res_g[g] for t=1:T, g=1:G)
+            - sum(up_e[t,w] * c_res_e[w] for t=1:T, w=1:2)
+            - sum(down_e[t,w] * c_res_e[w] for t=1:T, w=1:2)
+)
 
 #Capacity Limits
 @constraint(FN,[t=1:T,d=1:D], p_d[t,d] <= Cap_d[d]) #Demand limits constraint
-@constraint(FN,[t=1:T,g=1:G], p_g[t,g] <= Cap_g[g]) #Generation limits constraint
+@constraint(FN,[t=1:T,g=1:G], up_g[t,g] < p_g[t,g] <= Cap_g[g] - down_g[t,g]) #Generation limits constraint
 @constraint(FN,[t=1:T,w=1:W], p_w_grid_DA[t,w] + p_w_H2_DA[t,w] <= WF_prod[t,w]) #Weather-based limits constraint WF from DA market
+@constraint(FN,[t=1:T,w=1:2], down_e[t,w] <= p_w_H2_DA[t,w] <= WF_cap[w]/2 - up_e[t,w]) # Electrolyzer can maximum do max capacity - reserve and must do min reserve down
 
 #Power Balance
-@constraint(FN, Balance[t=1:T,a=1:A], sum(p_d[t,d]*psi_d[d,n] for d=1:D, n=1:N if psi_n[n,a]==1) 
-                                + sum(f[t,a,b] for b=1:A if ATC[a,b]>0) 
-                                - sum(p_w_grid_DA[t,w]*psi_w[w,n] for w=1:W, n=1:N if psi_n[n,a]==1) 
-                                - sum(p_g[t,g]*psi_g[g,n] for g=1:G, n=1:N if psi_n[n,a]==1)
-                                ==0)
+@constraint(FN, Balance[t=1:T], sum(p_d[t,d] for d=1:D) - sum(p_w_grid[t,w] for w=1:W) - sum(p_g[t,g] for g=1:G)==0) #Power balance constraint
 
 #Ramping up and down constraints
 @constraint(FN,[t=1:T,g=1:G], p_g[t,g] <= (t-1<1 ? Cap_g_init[g] : p_g[t-1,g]) + Ramp_g_u[g]) #ramp up constraint
 @constraint(FN,[t=1:T,g=1:G], p_g[t,g] >= (t-1<1 ? Cap_g_init[g] : p_g[t-1,g]) - Ramp_g_d[g]) #ramp down constraint
-
-#Power Flow constraints
-@constraint(FN,[t=1:T,a=1:A,b=1:A],f[t,a,b]<=ATC[a,b]) # Max ATC connecting zones a to b
-@constraint(FN,[t=1:T,a=1:A,b=1:A],f[t,a,b]>=-ATC[b,a]) # Min ATC connecting zones a to b
-@constraint(FN,[t=1:T,a=1:A,b=1:A],f[t,a,b]==-f[t,b,a]) #Symmetrical flow
 
 #Electrolyzer constraints
 @constraint(FN,[t=1:T, w=1:2], 0.01*(WF_cap[w]/2) <= p_w_H2_DA[t,w] <= WF_cap[w]/2)
