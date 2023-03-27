8bc1142783f2905477fdfd6c6b46ccba1d7e152c
NiklasLuettringhaus <niklas.luettringhaus@gmail.com>
2023-03-27 16:11:58 +0200

    Update scenarios.jl

--- Part 2/scenarios.jl
+++ Part 2/scenarios.jl
@@ -1,4 +1,18 @@
 using CSV
-using DataFrame
+using DataFrames
+using Random
+using Distributions
 
-CSV.read(file, DataFrame)
+#Set seed for reproducibility
+Random.seed!(123)
+
+#Generate 3 scenarios with 24 random binary variables
+power_balance1 = rand(Bernoulli(0.5), 24)
+power_balance2 = rand(Bernoulli(0.5), 24)
+power_balance3 = rand(Bernoulli(0.5), 24)
+
+#Append them all in a matrix
+matrix=hcat(power_balance1,power_balance2,power_balance3)
+
+# print the result
+println(matrix)
