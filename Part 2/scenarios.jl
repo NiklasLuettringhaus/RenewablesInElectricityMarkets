67578c8416c0e00356659a00d83a5f3d946306ca
NiklasLuettringhaus <niklas.luettringhaus@gmail.com>
2023-03-27 16:25:44 +0200

    Update scenarios.jl

--- Part 2/scenarios.jl
+++ Part 2/scenarios.jl
@@ -19,12 +19,10 @@ power_balance3 = rand(Bernoulli(0.5), 24)
 #Append them all in a matrix
 matrix=hcat(power_balance1,power_balance2,power_balance3)
 
-#creating an empty dictionary
 State_Dictionary = Dict()
-
 for i in 1:size(matrix, 2)
     # Create a string key for the dictionary
-    key = string(i)
+    key = string(i) * "S"
     
     # Convert the column to a string array and join it with commas
     value = join(string.(matrix[:, i]), ",")
@@ -32,6 +30,7 @@ for i in 1:size(matrix, 2)
     # Add the key-value pair to the dictionary
     State_Dictionary[key] = value
 end
+
 # print the result
-display(matrix)
+display(State_Dictionary)
 
