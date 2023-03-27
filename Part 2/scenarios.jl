b0013b397a460ad9d0976bb679e0462f888b6904
DavidMench <124593762+DavidMench@users.noreply.github.com>
2023-03-27 17:15:41 +0200

    wind_data_frame

--- Part 2/scenarios.jl
+++ Part 2/scenarios.jl
@@ -45,4 +45,7 @@ display(State_Dictionary)
 data_price = CSV.read("..\\RenewablesInElectricityMarkets\\Part 2\\Scenario_files\\Input_Prices.csv", DataFrame)
 show(data_price)
 
+data_wind= CSV.read("..\\RenewablesInElectricityMarkets\\Part 2\\Scenario_files\\Wind_scenarios.csv", DataFrame)
+show(data_wind)
+
 data_price[:, "s6"]
