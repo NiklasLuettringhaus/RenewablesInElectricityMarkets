feec3d6dea46325defaa0150e15f6421cc958518
juanfebbk <s184356@student.dtu.dk>
2023-03-27 16:30:47 +0200

    Update scenarios.jl

--- Part 2/scenarios.jl
+++ Part 2/scenarios.jl
@@ -17,15 +17,15 @@ power_balance2 = rand(Bernoulli(0.5), 24)
 power_balance3 = rand(Bernoulli(0.5), 24)
 
 #Append them all in a matrix
-matrix=hcat(power_balance1,power_balance2,power_balance3)
+statedata=hcat(power_balance1,power_balance2,power_balance3)
 
 State_Dictionary = Dict()
-for i in 1:size(matrix, 2)
+for i in 1:size(statedata, 2)
     # Create a string key for the dictionary
     key = string(i) * "S"
     
     # Convert the column to a string array and join it with commas
-    value = join(string.(matrix[:, i]), ",")
+    value = join(string.(statedata[:, i]), ",")
     
     # Add the key-value pair to the dictionary
     State_Dictionary[key] = value
