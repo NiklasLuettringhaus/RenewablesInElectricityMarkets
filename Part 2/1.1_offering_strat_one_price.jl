08e4c4257473a9a8734508cfded385ae785f910d
Pauline <106803877+thuene163@users.noreply.github.com>
2023-03-31 15:11:04 +0200

    Very important shit

---
+++ Part 2/1.1_offering_strat_one_price.jl
@@ -0,0 +1,54 @@
+#************************************************************************
+# Step 1.1
+using JuMP
+using Gurobi
+using Plots
+#************************************************************************
+
+#************************************************************************
+#PARAMETERS
+include("A2_data_step_1.1.jl")
+
+#************************************************************************
+
+#************************************************************************
+# Model
+A2_11 = Model(Gurobi.Optimizer)
+
+@variable(A2_11, p_DA[t=1:T]>=0)                #production sold in DA market
+@variable(A2_11, delta_up[t=1:T, s=1:S]>=0)     #up balancing sold in balancing market
+@variable(A2_11, delta_down[t=1:T, s=1:S]>=0)   #down balancing sold in balancing market
+@variable(A2_11, delta[t=1:T, s=1:S]>=0)        #total balancing in real time needed in balancing market
+
+@objective(A2_11, Max, sum(prob[s] *    (lambda_DA[t,s] * p_DA[t] 
+                                        + 0.9 * lambda_DA[t,s] * delta_up[t,s] 
+                                        - 1.2 * lambda_DA[t,s] * delta_down[t,s]) for t=1:T, s in generated_values))
+
+
+@constraint(A2_11,[t=1:T], 0 <= p_DA[t] <= p_nom)                                                       #capacity limit constraint for WF
+@constraint(A2_11,[t=1:T, s in generated_values], delta[t,s] == scenarios[s][1][t]*p_nom - p_DA[t])     #balancing need
+@constraint(A2_11,[t=1:T, s in generated_values], delta_up[t,s] - delta_down[t,s] == delta[t,s])        #composition of balancing
+@constraint(A2_11,[t=1:T], 0 <= p_DA[t] <= p_nom)
+
+#************************************************************************
+# Solve
+solution = optimize!(A2_11)
+println("Termination status: $(termination_status(A2_11))")
+#************************************************************************
+
+#************************************************************************
+# Solution
+if termination_status(A2_11) == MOI.OPTIMAL
+    println("Optimal objective value: $(objective_value(A2_11))")
+    println("Solution: ")
+    DA_price = -dual.(Balance) #Equilibrium price
+    println("Market clearing price:")
+    println(DA_price)  #Print equilibrium price
+    println("\n")
+    println("Profit of each generator:")
+    for g=1:G
+        println("G$g:", round(Int,value(p_g[g])*(DA_price - C_g[g])))
+    end
+else
+    println("No optimal solution available")
+end
---
+++ Part 2/A2_data_step_1.1.jl
@@ -0,0 +1,82 @@
+#Assignment 2, Data file Step 1.1
+
+using CSV
+using DataFrames
+using Random
+using Distributions
+
+
+#*****************************************
+# SCENARIO GENERATION
+#*****************************************
+
+data_price = CSV.read("Part 2/Scenario_files/Input_Prices.csv", DataFrame)
+data_states = CSV.read("Part 2/Scenario_files/State_scenarios.csv", DataFrame)
+data_wind = CSV.read("Part 2/Scenario_files/Wind_scenarios.csv", DataFrame)
+
+data_states_dict = Dict(i => vec(c) for (i, c) in enumerate(eachcol(data_states)) if i <= 3)
+data_price_dict = Dict(i => vec(c) for (i, c) in enumerate(eachcol(data_price)) if i <= 20)
+data_wind_dict = Dict(i => vec(c) for (i, c) in enumerate(eachcol(data_wind)) if i <= 10)
+
+#Create an empty main dictionary for all 600 scnearios
+scenarios = Dict()
+#loop through each dictionary for wind, power and state. Write the keys of each as a string to create a unique key that lets us identify each scenario and grab the values (arrays) of each dictionary
+#=
+for w in 1:length(data_wind_dict)
+    for p in 1:length(data_price_dict)
+        for s in 1:length(data_states_dict)
+            scenarios_dict["w$w p$p s$s"] = [get(data_wind_dict, w,0), get(data_price_dict, p,0),get(data_states_dict, s,0) ]
+        end
+    end
+end
+=#
+
+i_d=1
+for w in 1:length(data_wind_dict)
+    global i_d
+    for p in 1:length(data_price_dict)
+        for s in 1:length(data_states_dict)
+            scenarios[i_d] = [get(data_wind_dict, w,0), get(data_price_dict, p,0),get(data_states_dict, s,0) ]
+            i_d+=1
+        end
+    end
+end
+
+#sort it so it is easier to see
+sorted_scenarios = sort(scenarios)
+CSV.write("Part 2/Scenario_files/sorted_scenarios_dict.csv", sorted_scenarios_dict)
+
+#*****************************************
+# REDUCING SCENARIOS
+#*****************************************
+
+n=200
+
+# Create an empty set to store the values
+set_values = Set{Int}()
+
+# Generate random values until the set contains n unique values
+while length(set_values) < n
+    value = rand(1:600)
+    push!(set_values, value)
+end
+
+# Convert the set to an array and sort it
+generated_values = sort(collect(set_values))
+
+# Create an array with all possible values and remove the generated ones
+all_values = collect(1:600)
+new_values = setdiff(all_values, generated_values)
+
+for s in generated_values
+    println((scenarios[s]), "\n")
+end
+
+#*****************************************
+# PARAMETERS
+#*****************************************
+
+prob = fill(0.005,200)
+p_nom = 150 #MW
+S = length(generated_values)
+T = length(scenarios[1][1])
