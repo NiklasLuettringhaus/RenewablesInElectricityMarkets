b087cbb5d70603acbe49a8dbfe93b79d682e646f
DavidMench <124593762+DavidMench@users.noreply.github.com>
2023-03-31 13:14:44 +0200

    Update scenarios.jl

--- Part 2/scenarios.jl
+++ Part 2/scenarios.jl
@@ -3,7 +3,7 @@ using DataFrames
 using Random
 using Distributions
 
-
+#=
 #*****************************************
 #   State matrix
 #*****************************************
@@ -19,6 +19,7 @@ power_balance3 = rand(Bernoulli(0.5), 24)
 #Append them all in a matrix
 statedata=hcat(power_balance1,power_balance2,power_balance3)
 
+<<<<<<< HEAD
 State_Dictionary = Dict()
 for i in 1:size(statedata, 2)
     # Create a string key for the dictionary
@@ -90,14 +91,39 @@ for i in 1:size(statedata, 2)
     State_Dictionary[key] = value
 end
 
+=======
+>>>>>>> cb1d76cd4a204d8f5bf1e32e16f0cf8bc10e855f
 
 
 # print the result
 display(State_Dictionary)
+=#
 
-#*****************************************
-#   DA Price matrix
-#*****************************************
 
+<<<<<<< HEAD
 data_price = CSV.read("..\\RenewablesInElectricityMarkets\\Scenario_files\\Input_Prices.csv", DataFrame)
 >>>>>>> d97db2b1db2a9581367850ca7768c0d46f577c10
+=======
+data_price = CSV.read("Part 2/Scenario_files/Input_Prices.csv", DataFrame)
+data_states = CSV.read("Part 2/Scenario_files/State_scenarios.csv", DataFrame)
+data_wind = CSV.read("Part 2/Scenario_files/Wind_scenarios.csv", DataFrame)
+
+data_states_dict = Dict(i => vec(c) for (i, c) in enumerate(eachcol(data_states)) if i <= 3)
+data_price_dict = Dict(i => vec(c) for (i, c) in enumerate(eachcol(data_price)) if i <= 20)
+data_wind_dict = Dict(i => vec(c) for (i, c) in enumerate(eachcol(data_wind)) if i <= 10)
+
+#Create an empty main dictionary for all 600 scnearios
+scenarios_dict = Dict()
+#loop through each dictionary for wind, power and state. Write the keys of each as a string to create a unique key that lets us identify each scenario and grab the values (arrays) of each dictionary
+for w in 1:length(data_wind_dict)
+    for p in 1:length(data_price_dict)
+        for s in 1:length(data_states_dict)
+            scenarios_dict["w$w p$p s$s"] = [get(data_wind_dict, w,0), get(data_price_dict, p,0),get(data_states_dict, s,0) ]
+        end
+    end
+end
+
+#sort it so it is easier to see
+sorted_scenarios_dict = sort(scenarios_dict)
+CSV.write("Part 2/Scenario_files/sorted_scenarios_dict.csv", sorted_scenarios_dict)
+>>>>>>> cb1d76cd4a204d8f5bf1e32e16f0cf8bc10e855f
