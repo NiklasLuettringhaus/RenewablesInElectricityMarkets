063fc2ea5fb112b74408eda8f34897bfad3062df
NiklasLuettringhaus <niklas.luettringhaus@gmail.com>
2023-04-04 10:16:19 +0200

    Update 1.4_risk_analysis_one_price.jl

--- Part 2/1.4_risk_analysis_one_price.jl
+++ Part 2/1.4_risk_analysis_one_price.jl
@@ -23,6 +23,10 @@ A2_11 = Model(Gurobi.Optimizer)
 @variable(A2_11, delta_down[t=1:T, s in generated_values]>=0)   #down balancing sold in balancing market
 @variable(A2_11, delta[t=1:T, s in generated_values]>=0)        #total balancing in real time needed in balancing market
 
+# Variables for CVAR
+@variable(A2_11, zetta[t = 1:T] )
+
 @objective(A2_11, Max, sum(prob[s] *    (scenarios[s][2][t] * p_DA[t] 
                                         + (1-scenarios[s][3][t]) * 1.2 * scenarios[s][2][t] * (delta_up[t,s] - delta_down[t,s])
-                                        + scenarios[s][3][t] * 0.9 * scenarios[s][2][t] * (delta_up[t,s] - delta_down[t,s])) for t=1:T, s in generated_values)- beta )
+                                        + scenarios[s][3][t] * 0.9 * scenarios[s][2][t] * (delta_up[t,s] - delta_down[t,s])) for t=1:T, s in generated_values)
+                                        - beta * (zeta - 1/(1-alpha) * prob[s]))
